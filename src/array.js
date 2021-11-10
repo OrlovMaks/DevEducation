894a5c16f20f0578cbd4a50e5eeeaa53dc884358
MaksOrlov <4014499@gmail.com>
2021-11-10 13:46:05 +0200

    rewrite HW for functions

--- src/array.js
+++ src/array.js
@@ -1,74 +1,88 @@
 // 1. Найти минимальный элемент массива
-let minNum = [5, 2, 40, 27, 17, 3];
-let min = minNum[0];
+function minNum (minArray){
+    let min = minArray[0];
 
-for (let i = 1; i < minNum.length; i++) {
-    if (min > minNum[i]) {
-        min = minNum[i];
+for (let i = 1; i < minArray.length; i++) {
+    if (min > minArray[i]) {
+        min = minArray[i];
     }
 
 }
-console.log(min);
+return min;
+}
+console.log(minNum([5, 2, 40, 27, 17, 3]))
 
 // 2. Найти максимальный элемент массива
-let maxNum = [5, 2, 40, 27, 17, 3];
-let max = maxNum[0];
+function maxNum (maxArray){
+    let max = maxArray[0];
 
-for (let i = 1; i < maxNum.length; i++) {
-    if (max < maxNum[i]) {
-        max = maxNum[i];
+for (let i = 1; i < maxArray.length; i++) {
+    if (max < maxArray[i]) {
+        max = maxArray[i];
     }
 
 }
-console.log(max);
+return max;
+}
+console.log(maxNum([5, 2, 40, 27, 17, 3]));
 
 // 3. Найти индекс минимального элемента массива
-let minInd = [5, 2, 40, 27, 17, 3];
-let minN = minInd[0];
-let minIndex;
+function minimumIndex (array){
+    let minN = array[0];
+    let minIndex;
+for (let i = 1; i < array.length; i++) {
 
-for (let i = 1; i < minInd.length; i++) {
-    if (minN > minInd[i]) {
-        minN = minInd[i];
+    if (minN > array[i]) {
+        minN = array[i];
         minIndex = i;
     }
 
 }
-console.log(minIndex);
+return minIndex;
+}
+console.log(minimumIndex([5, 7, 40, 2, 17, 3]))
 
 // 4. Найти индекс максимального элемента массива
-let maxInd = [5, 2, 40, 27, 17, 3];
-let maxN = maxInd[0];
+function maximumIndex(array){
+let maxN = array[0];
 let maxIndex;
 
-for (let i = 1; i < maxInd.length; i++) {
-    if (maxN < maxInd[i]) {
-        maxN = maxInd[i];
+for (let i = 1; i < array.length; i++) {
+    if (maxN < array[i]) {
+        maxN = array[i];
         maxIndex = i;
     }
 
 }
-console.log(maxIndex);
+return maxIndex;
+}
+console.log(maximumIndex([5, 2, 40, 27, 17, 3]))
 
 // 5. Посчитать сумму элементов массива с нечетными индексами
-let oddArray = [3, 6, 24, 78, 36, 49, 7, 11, 1];
+function oddArray(array) {
 let oddSum = 0;
-for (let i = 1; i < oddArray.length; i = i + 2) {
-    oddSum = oddSum + oddArray[i];
+for (let i = 1; i < array.length; i = i + 2) {
+    oddSum = oddSum + array[i];
 }
-console.log(oddSum);
+return oddSum;
+}
+console.log(oddArray([3, 6, 24, 78, 36, 49, 7, 11, 1]))
 
-// 6. Сделать реверс массива (массив в обратном направлении)
-let revArray = [1, 2, 3, 4, 5];
-revArray.reverse();
-console.log(revArray);
+// 6. Сделать реверс массива (массив в обратном направлении)[1, 2, 3, 4, 5];
+function revArray(array){
+    array.reverse();
+    return (array);
+}
+console.log(revArray([1, 2, 3, 4, 5]))
 
 // 7. Посчитать количество нечетных элементов массива
-let oddElArray = [2, 3, 5, 7, 4, 6, 1];
+function oddElArray(array){
 let counter = 0;
-for (let i = 0; i < oddElArray.length; i++) {
-    if (oddElArray[i] % 2 != 0) {
+for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 != 0) {
         counter++;
     }
 }
-console.log(counter);
+return counter;
+}
+console.log(oddArray([2, 3, 5, 7, 4, 6, 1]))
--- src/cycle.js
+++ src/cycle.js
@@ -1,13 +1,15 @@
 // 1. Найти сумму четных чисел и их количество в диапазоне от 1 до 99
-let res = 0;
-for (let i = 0; i < 99; i = i + 2) {
+function sumEven(res){
+    for (let i = 0; i < 99; i = i + 2) {
     res = res + i;
 }
-console.log(res);
+return res
+}
+console.log(sumEven(0))
 
 /* 2. Проверить простое ли число? (число называется простым, если оно делится только
 само на себя и на 1)*/
-let num = 5;
+function checkNum (num){
 let flag = true;
 for (let i = 2; i < num; i++) {
     if (num % i == 0) {
@@ -15,48 +17,59 @@ for (let i = 2; i < num; i++) {
         break;
     }
 }
-
-console.log(flag);
+return flag;
+}
+console.log(checkNum(5))
 
 /* 3. Найти корень натурального числа с точностью до целого (рассмотреть вариант
 последовательного подбора и метод бинарного поиска)*/
-let n = 9, r;
-for (let i = 1; i < n; i++) {
-    if (i * i === n) {
+function rootNum (num){
+    let r;
+for (let i = 1; i < num; i++) {
+    if (i * i === num) {
         r = i;
         break;
     }
 
 }
 if (r === undefined) {
-    console.log('Целого корня нет!');
+    return ('Целого корня нет!');
 }
-else console.log(r);
+else return (r);
+}
+console.log(rootNum(9))
 
 // 4. Вычислить факториал числа n. n! = 1*2*…*n-1*n;
-let f = 3, fact = 1;
+function factNum (f, fact){
 for (let i = 1; i <= f; i++) {
-    fact = fact * i;
+fact = fact * i;
 }
-console.log(fact);
+return fact;
+}
+console.log(factNum(3, 1))
 
 // 5. Посчитать сумму цифр заданного числа
-let a = 352, sum = 0, tmp;
+function sumNum(a){
+    let sum = 0, tmp;
 while (a > 0) {
     tmp = a % 10;
     sum = sum + tmp;
     a = (a - tmp) / 10;
 }
-console.log(sum);
+return (sum);
+}
+console.log(sumNum(352))
 
 /* 6. Вывести число, которое является зеркальным отображением последовательности
-цифр заданного числа, например, задано число 123, вывести 321.*/
-let b = 1234, reverse = 0, temp, i = 1000;
+цифр заданного числа, например, задано число 123, вывести 321.*/ 
+function reversNum (b){
+let reverse = 0, temp, i = 1000;
 while (b) {
     temp = b % 10;
     reverse += temp * i;
     i = i / 10;
     b = (b - temp) / 10;
 }
-
-console.log(reverse);
+return reverse
+}
+console.log(reversNum(1234))

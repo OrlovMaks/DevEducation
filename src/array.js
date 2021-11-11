88bc515f499c134dad2b9a2e8232c36e274af18e
OrlovMaks <4014499@gmail.com>
2021-11-11 21:06:13 +0200

    done HW tests for data types

--- src/array.js
+++ src/array.js
@@ -1,88 +1,88 @@
 // 1. Найти минимальный элемент массива
-function minNum (minArray){
+function minNum(minArray) {
     let min = minArray[0];
 
-for (let i = 1; i < minArray.length; i++) {
-    if (min > minArray[i]) {
-        min = minArray[i];
-    }
+    for (let i = 1; i < minArray.length; i++) {
+        if (min > minArray[i]) {
+            min = minArray[i];
+        }
 
-}
-return min;
+    }
+    return min;
 }
 console.log(minNum([5, 2, 40, 27, 17, 3]))
 
 // 2. Найти максимальный элемент массива
-function maxNum (maxArray){
+function maxNum(maxArray) {
     let max = maxArray[0];
 
-for (let i = 1; i < maxArray.length; i++) {
-    if (max < maxArray[i]) {
-        max = maxArray[i];
-    }
+    for (let i = 1; i < maxArray.length; i++) {
+        if (max < maxArray[i]) {
+            max = maxArray[i];
+        }
 
-}
-return max;
+    }
+    return max;
 }
 console.log(maxNum([5, 2, 40, 27, 17, 3]));
 
 // 3. Найти индекс минимального элемента массива
-function minimumIndex (array){
+function minimumIndex(array) {
     let minN = array[0];
     let minIndex;
-for (let i = 1; i < array.length; i++) {
+    for (let i = 1; i < array.length; i++) {
 
-    if (minN > array[i]) {
-        minN = array[i];
-        minIndex = i;
+        if (minN > array[i]) {
+            minN = array[i];
+            minIndex = i;
+        }
+
     }
-
-}
-return minIndex;
+    return minIndex;
 }
 console.log(minimumIndex([5, 7, 40, 2, 17, 3]))
 
 // 4. Найти индекс максимального элемента массива
-function maximumIndex(array){
-let maxN = array[0];
-let maxIndex;
+function maximumIndex(array) {
+    let maxN = array[0];
+    let maxIndex;
 
-for (let i = 1; i < array.length; i++) {
-    if (maxN < array[i]) {
-        maxN = array[i];
-        maxIndex = i;
+    for (let i = 1; i < array.length; i++) {
+        if (maxN < array[i]) {
+            maxN = array[i];
+            maxIndex = i;
+        }
+
     }
-
-}
-return maxIndex;
+    return maxIndex;
 }
 console.log(maximumIndex([5, 2, 40, 27, 17, 3]))
 
 // 5. Посчитать сумму элементов массива с нечетными индексами
 function oddArray(array) {
-let oddSum = 0;
-for (let i = 1; i < array.length; i = i + 2) {
-    oddSum = oddSum + array[i];
-}
-return oddSum;
+    let oddSum = 0;
+    for (let i = 1; i < array.length; i = i + 2) {
+        oddSum = oddSum + array[i];
+    }
+    return oddSum;
 }
 console.log(oddArray([3, 6, 24, 78, 36, 49, 7, 11, 1]))
 
 // 6. Сделать реверс массива (массив в обратном направлении)[1, 2, 3, 4, 5];
-function revArray(array){
+function revArray(array) {
     array.reverse();
     return (array);
 }
 console.log(revArray([1, 2, 3, 4, 5]))
 
 // 7. Посчитать количество нечетных элементов массива
-function oddElArray(array){
-let counter = 0;
-for (let i = 0; i < array.length; i++) {
-    if (array[i] % 2 != 0) {
-        counter++;
+function oddElArray(array) {
+    let counter = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 != 0) {
+            counter++;
+        }
     }
+    return counter;
 }
-return counter;
-}
-console.log(oddArray([2, 3, 5, 7, 4, 6, 1]))
+console.log(oddElArray([2, 3, 5, 7, 4, 6, 1]))
--- src/cycle.js
+++ src/cycle.js
@@ -1,75 +1,75 @@
 // 1. Найти сумму четных чисел и их количество в диапазоне от 1 до 99
-function sumEven(res){
+function sumEven(res) {
     for (let i = 0; i < 99; i = i + 2) {
-    res = res + i;
-}
-return res
+        res = res + i;
+    }
+    return res
 }
 console.log(sumEven(0))
 
 /* 2. Проверить простое ли число? (число называется простым, если оно делится только
 само на себя и на 1)*/
-function checkNum (num){
-let flag = true;
-for (let i = 2; i < num; i++) {
-    if (num % i == 0) {
-        flag = false;
-        break;
+function checkNum(num) {
+    let flag = true;
+    for (let i = 2; i < num; i++) {
+        if (num % i == 0) {
+            flag = false;
+            break;
+        }
     }
-}
-return flag;
+    return flag;
 }
 console.log(checkNum(5))
 
 /* 3. Найти корень натурального числа с точностью до целого (рассмотреть вариант
 последовательного подбора и метод бинарного поиска)*/
-function rootNum (num){
+function rootNum(num) {
     let r;
-for (let i = 1; i < num; i++) {
-    if (i * i === num) {
-        r = i;
-        break;
-    }
+    for (let i = 1; i < num; i++) {
+        if (i * i === num) {
+            r = i;
+            break;
+        }
 
+    }
+    if (r === undefined) {
+        return ('Целого корня нет!');
+    }
+    else return (r);
 }
-if (r === undefined) {
-    return ('Целого корня нет!');
-}
-else return (r);
-}
-console.log(rootNum(9))
+console.log(rootNum(5))
 
 // 4. Вычислить факториал числа n. n! = 1*2*…*n-1*n;
-function factNum (f, fact){
-for (let i = 1; i <= f; i++) {
-fact = fact * i;
+function factNum(f, fact) {
+    for (let i = 1; i <= f; i++) {
+        fact = fact * i;
+    }
+    return fact;
 }
-return fact;
-}
-console.log(factNum(3, 1))
+console.log(factNum(5, 1))
 
 // 5. Посчитать сумму цифр заданного числа
-function sumNum(a){
+function sumNum(a) {
     let sum = 0, tmp;
-while (a > 0) {
-    tmp = a % 10;
-    sum = sum + tmp;
-    a = (a - tmp) / 10;
-}
-return (sum);
+    while (a > 0) {
+        tmp = a % 10;
+        sum = sum + tmp;
+        a = (a - tmp) / 10;
+    }
+    return (sum);
 }
 console.log(sumNum(352))
 
 /* 6. Вывести число, которое является зеркальным отображением последовательности
-цифр заданного числа, например, задано число 123, вывести 321.*/ 
-function reversNum (b){
-let reverse = 0, temp, i = 1000;
-while (b) {
-    temp = b % 10;
-    reverse += temp * i;
-    i = i / 10;
-    b = (b - temp) / 10;
-}
-return reverse
+цифр заданного числа, например, задано число 123, вывести 321.*/
+function reversNum(b) {
+    let reverse = 0, temp, i = 1000;
+    while (b) {
+        temp = b % 10;
+        reverse += temp * i;
+        i = i / 10;
+        b = (b - temp) / 10;
+    }
+    return reverse
 }
 console.log(reversNum(1234))

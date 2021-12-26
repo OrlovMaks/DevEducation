eb02cb59405b289d2c6279416c86dd9f29fdf00a
OrlovMaks <4014499@gmail.com>
2021-12-26 18:39:12 +0200

    clear brunch

--- gallery_V2/src/ts/constants.ts
+++
@@ -1,2 +0,0 @@
-export let currentPage = 1;
-export const elements = 9;
--- gallery_V2/src/ts/index.ts
+++
@@ -1,6 +0,0 @@
-import '../css/style.css';
-import { mainLogic } from './functions';
-import '../css/style.css';
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-mainLogic()
